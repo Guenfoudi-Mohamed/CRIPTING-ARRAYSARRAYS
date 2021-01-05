4417502602d51564dae653f00b59ab44a41ccb34
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2021-01-05 15:23:53 +0100

    Update CRIPTING_ARRAYS.js

--- CRIPTING_ARRAYS.js
+++ CRIPTING_ARRAYS.js
@@ -1,10 +1,10 @@
                                                                             /*crypting for << ARRAYS*/ 
 
 var m="043",maj="034",dim="555"
-var list_1 = ["M-","O-","H-","A-","M-","E-","D-"];
+var list_1 = [" A"," A#"," B"," C"," C#"," D"," D#"," E"," F"," F#"," G"," G#"];
 var list_2 = [m,maj,dim];
 var v2="";var v1;
-
+document.write(list_2,"<br>",list_1);
 var list = function(n1,n2){                                                          /*<<< function expression*/
     if(!isNaN(n1) & !isNaN(n2)){
         if((n1 > 0 && n1 <= list_1.length) && (n2 > 0 && n2 <= list_2.length)){
@@ -16,6 +16,11 @@ var list = function(n1,n2){                                                     
                     var o1 = -1;
                     for(var t1 = 0;t1 < list_2[n2].length ;t1++){
                         for(v1 = o1;v1 < list_2[n2][t1];v1++){
+                            if(n1 == list_1.length-1){
+                                v2 +=list_1[n1];    
+                                n1=0;
+                                continue;
+                            }
                             v2 +=list_1[n1];
                             n1+=1;   
                         }
@@ -50,4 +55,4 @@ var list = function(n1,n2){                                                     
         document.write("<Mark style=font-family:'consolas';font-size:20px;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);text-align:center;>INCORECT n1 or n2 is not a nomber TRY AGAINE!!</mark>");
     }
 }
-list(1,1);    /*enter here a number list(num,num);*/
+list(12,1);    /*enter here a number list(num,num);*/
